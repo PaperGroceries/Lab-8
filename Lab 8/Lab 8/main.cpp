f6c729d890e2c8dd61efb62019bc20c7c68f5363
PaperGroceries <106277251+PaperGroceries@users.noreply.github.com>
2022-07-25 13:17:38 -0700

    start

---
+++ Lab 8/Lab 8/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 8
+//
+//  Created by Aim Kim on 2022-07-25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
